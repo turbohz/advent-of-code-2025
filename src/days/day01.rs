de11f92285ceda3ff36c671fca5b9e0416c44234
TurboHz <turbohz@gmail.com>
2025-12-05 13:07:37 +0100

    Implements day01 (part 2)

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,19 +1,30 @@
 // https://adventofcode.com/2025/day/1
 
 use super::*;
-use num::Zero;
+use num::{Zero, zero};
 
+#[derive(Clone, Copy)]
 enum Dir { L, R }
 
+#[derive(Clone, Copy)]
 struct Stride {
 	dir: Dir,
 	dis: usize
 }
 
-impl From<Stride> for isize {
-	fn from(Stride { dir, dis } : Stride) -> Self {
-		let res = dis as isize;
-		if matches!(dir,Dir::L) { -res } else { res }
+impl Stride {
+	fn as_raw(&self) -> isize {
+		let res = self.dis as isize;
+		if matches!(self.dir,Dir::L) { -res } else { res }
+	}
+
+	fn into_raw(self) -> isize {
+		self.as_raw()
+	}
+
+	fn from_raw(d:isize) -> Stride {
+		let dir = if d >= 0 { Dir::R } else { Dir::L };
+		Stride { dir, dis: d.abs() as usize }
 	}
 }
 
@@ -25,13 +36,15 @@ impl Default for Dial {
 	}
 }
 
-
 impl Dial {
 
 	fn turn(&mut self, s:Stride) {
-		let t = isize::from(s);
+
+		let s = s.as_raw();
 		let v = self.0 as isize;
-		self.0 = (v+t).rem_euclid(100) as usize;
+
+
+		self.0 = (v+s).rem_euclid(100) as usize;
 	}
 
 	fn value(&self) -> usize {
@@ -72,7 +85,10 @@ impl Solution for Part1 {
 		let strides = parse(input,parser::stride);
 
 		let zeroes = strides
-			.map(|s| { dial.turn(s); dial.value() })
+			.map(|s| {
+				dial.turn(s);
+				dial.value()
+			})
 			.filter(Zero::is_zero)
 			.count();
 
@@ -80,10 +96,66 @@ impl Solution for Part1 {
 	}
 }
 
+struct Part2;
+
+impl Solution for Part2 {
+
+	const DAY: i32 = 1;
+	const PART: Part = Part::Part2;
+
+	fn solve(input:&str) -> impl Display {
+
+		let mut dial = Dial::default();
+		let strides = parse(input,parser::stride);
+
+		let mut zero_crossed:usize = 0;
+
+		// Returns a "reduced" stride, and the amount of full revolutions removed
+		fn simplify(s:Stride) -> (Stride,usize) {
+			(Stride { dis: s.dis % 100,..s },s.dis / 100)
+		}
+
+		for s in strides {
+
+			let (s, full_revs) = simplify(s);
+
+			// Account for full rotations crossing by zero
+			zero_crossed += full_revs;
+
+			let prev = dial.value();
+
+			dial.turn(s);
+
+			let next = dial.value();
+
+			// Any simplified stride that started from 0 cannot **cross** by zero.
+			// Otherwise ..
+			if prev > 0 {
+
+				let inc = if next == 0 {
+					1
+				} else {
+
+					match s.dir {
+						Dir::L if next > prev => 1,
+						Dir::R if next < prev => 1,
+						_ => 0
+					}
+				};
+
+				zero_crossed += inc;
+			}
+		}
+
+		zero_crossed
+	}
+}
+
 #[cfg(test)]
 mod test {
 
 	use super::*;
+	use rstest::*;
 
 	const EXAMPLE_INPUT:&str = indoc! {
 	r#"
@@ -105,7 +177,7 @@ mod test {
 		let turns = parse(EXAMPLE_INPUT,parser::stride);
 
 		let expected = vec![ -68, -30, 48, -5, 60, -55, -1,-99, 14, -82 ];
-		let actual:Vec<isize> = turns.map(Into::into).collect();
+		let actual:Vec<isize> = turns.map(Stride::into_raw).collect();
 
 		assert_eq!(actual,expected);
 	}
@@ -133,12 +205,18 @@ mod test {
 		let expected = "3";
 		let actual = Part1::solve(EXAMPLE_INPUT).to_string();
 
-		assert_eq!(actual,expected)
+		assert_eq!(actual,expected);
+
+		let expected = "6";
+		let actual = Part2::solve(EXAMPLE_INPUT).to_string();
+
+		assert_eq!(actual,expected);
 	}
 
 	#[test]
 	fn submit()-> Result<(), AppError> {
 		Part1::try_submit()?;
+		Part2::try_submit()?;
 		Ok(())
 	}
 }
