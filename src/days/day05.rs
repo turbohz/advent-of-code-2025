69b63eaa6ffda65daf615b67df7558753067f5f5
TurboHz <turbohz@gmail.com>
2025-12-15 13:07:41 +0100

    Implements day05 (part 2)

--- src/days/day05.rs
+++ src/days/day05.rs
@@ -2,7 +2,7 @@
 
 use super::*;
 
-use std::ops::RangeInclusive;
+use std::{fmt::Debug, ops::RangeInclusive};
 
 peg::parser! {
 	grammar parser() for str {
@@ -44,6 +44,104 @@ impl Solution for Part1 {
 		ids.filter(|id| {
 			ranges.iter().any(|r| r.contains(id))
 		}).count()
+	}
+}
+
+#[derive(Clone,Copy)]
+struct FreshRange {
+	fst: usize,
+	lst: usize,
+}
+
+impl Debug for FreshRange {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		let ran:RangeInclusive<usize> = (*self).into();
+		ran.fmt(f)
+	}
+}
+
+impl From<FreshRange> for RangeInclusive<usize> {
+	fn from(ran: FreshRange) -> Self {
+		ran.fst..=ran.lst
+	}
+}
+
+impl From<RangeInclusive<usize>> for FreshRange {
+	fn from(ran: RangeInclusive<usize>) -> Self {
+		Self {
+			fst: *ran.start(),
+			lst: *ran.end()
+		}
+	}
+}
+
+impl FreshRange {
+
+	fn len(&self) -> usize {
+		1 + self.lst - self.fst
+	}
+
+	fn unchecked_try_merge(&self,rhs:&FreshRange) -> Option<Self> {
+		use std::cmp::max;
+
+		assert!(self.fst <= rhs.fst);
+
+		if (1 + self.lst) >= rhs.fst {
+			// Merge
+			let lst = max(self.lst,rhs.lst);
+			Some(FreshRange { lst, ..*self })
+		} else {
+			None
+		}
+	}
+}
+
+struct Part2;
+
+impl Solution for Part2 {
+
+	const DAY: i32 = 5;
+	const PART: Part = Part::Part2;
+
+	fn solve(input:&str) -> impl Display {
+
+		let (ranges,_) = parse(input);
+
+		let mut ranges = ranges
+			.into_iter()
+			.map(FreshRange::from)
+			.sorted_unstable_by(|a,b| a.fst.cmp(&b.fst));
+
+		#[derive(Debug,Default)]
+		struct State {
+			acc: usize,
+			ran: Option<FreshRange>
+		};
+
+
+		impl State {
+			fn into_count(self) -> usize {
+				self.acc + self.ran.as_ref().map_or(0, FreshRange::len)
+			}
+		}
+
+		ranges.fold(State::default(), |mut st,right| {
+			if let Some(left) = st.ran {
+				if let Some(merged) = left.unchecked_try_merge(&right) {
+					st.ran = Some(merged)
+				} else {
+					// count left and accumulate
+					st.acc += left.len();
+					st.ran = Some(right);
+				}
+			} else {
+				// First range, initialize st.ran
+				st.ran = Some(right)
+			}
+
+			st
+
+		}).into_count()
 	}
 }
 
@@ -90,13 +188,18 @@ mod test {
 
 	#[test]
 	fn test_example() {
+
 		let actual = Part1::solve(EXAMPLE_INPUT).to_string();
 		let expected = "3";
+		assert_eq!(actual,expected);
+
+		let actual = Part2::solve(EXAMPLE_INPUT).to_string();
+		let expected = "14";
 		assert_eq!(actual,expected);
 	}
 
 	// SOLUTIONS
 
 	submit! { Part1 }
-	// submit! { Part2 }
+	submit! { Part2 }
 }
